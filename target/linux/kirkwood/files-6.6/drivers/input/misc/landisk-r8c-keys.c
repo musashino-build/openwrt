57074d2d2c9b964fe501d7fc9a52efd2c323132c
INAGAKI Hiroshi <musashino.open@gmail.com>
2024-11-13 00:07:27 +0900

    kirkwood: improve R8C key driver to handle HDD switches on HDL-XV
    Add polled keys support for "wxyz" swithes of HDD slots.
    
    -----------------------
    This is a combination of 5 commits.
    
    kirkwood: remove linux,input-type support from R8C key driver
    
    kirkwood: add HDD switch support to R8C key driver
    
    kirkwood: use 1000ms as polling interval on R8C key driver
    
    kirkwood: fix count of HDD switches in R8C key driver
    
    kirkwood: separate "compatible" for buttons/hdds in R8C key driver
    
    kirkwood: fix "compatible" of -event type in R8C key driver
    
    kirkwood: add "poll-interval" support to R8C key driver
    
    kirkwood: set initial state of polled keys on R8C key driver
    
    kirkwood: check hdd codes for -event type in R8C key driver
    
    kirkwood: improve initial reporting of keys on R8C key driver

--- target/linux/kirkwood/files-6.6/drivers/input/misc/landisk-r8c-keys.c
+++ target/linux/kirkwood/files-6.6/drivers/input/misc/landisk-r8c-keys.c
@@ -10,29 +10,68 @@
 
 #include <linux/mfd/landisk-r8c.h>
 
+#define CMD_BTN			"btn"
+#define R8C_BTN_CHARS		7
+
+#define R8C_POLL_INTERVAL	1000
+
 struct r8c_key_device {
 	const char *name;
-	char evcode;
+	char r8kcode;
 	unsigned int lkcode;
+	unsigned int evtype;
+	bool last_state;
 };
 
 struct r8c_keys {
+	struct r8c_mcu *r8c;
 	struct input_dev *idev;
 	struct notifier_block nb;
-	char last_code;
 	int keycnt;
-	struct r8c_key_device keys[0];
+	struct r8c_key_device keys[];
 };
+
+static void r8c_key_poll(struct input_dev *idev)
+{
+	struct r8c_keys *k = input_get_drvdata(idev);
+	char buf[16];
+	int ret, i = 0;
+
+	ret = landisk_r8c_exec_cmd(k->r8c, CMD_BTN, NULL, buf, sizeof(buf));
+	if (ret != R8C_BTN_CHARS)
+		return;
+	/* example: "crpWxyZ" */
+	while (buf[i]) {
+		struct r8c_key_device *kdev = k->keys;
+		bool pressed = (buf[i] <= 'Z');
+
+		while (kdev < k->keys + k->keycnt) {
+			if (buf[i] != kdev->r8kcode - (pressed ? 0x20 : 0x0)) {
+				kdev++;
+				continue;
+			}
+
+			if (kdev->last_state == pressed)
+				break;
+			kdev->last_state = pressed;
+
+			input_event(idev, kdev->evtype, kdev->lkcode, pressed);
+			input_sync(idev);
+			break;
+		}
+		i++;
+	}
+}
 
 static int r8c_key_event(struct notifier_block *nb,
 			 unsigned long evcode, void *data)
 {
 	struct r8c_keys *k = container_of(nb, struct r8c_keys, nb);
-	const struct r8c_key_device *kdev = k->keys;
-	bool pressed = evcode <= 'Z' ? true : false;
+	struct r8c_key_device *kdev = k->keys;
+	bool pressed = evcode <= 'Z';
 
 	while (kdev < k->keys + k->keycnt) {
-		if (evcode != kdev->evcode - (pressed ? 0x20 : 0x0)) {
+		if (evcode != kdev->r8kcode - (pressed ? 0x20 : 0x0)) {
 			kdev++;
 			continue;
 		}
@@ -47,11 +86,11 @@ static int r8c_key_event(struct notifier_block *nb,
 		 *   'R' (pressed), 'r' (released), 'R' (pressed),
 		 *   -> 'R' (held), 'r' (released)
 		 */
-		if (pressed && k->last_code == evcode)
+		if (pressed && kdev->last_state == pressed)
 			return NOTIFY_STOP;
-		k->last_code = (char)evcode;
+		kdev->last_state = pressed;
 
-		input_report_key(k->idev, kdev->lkcode, pressed ? 1 : 0);
+		input_event(k->idev, kdev->evtype, kdev->lkcode, pressed);
 		input_sync(k->idev);
 
 		dev_dbg(&k->idev->dev, "%s %s\n",
@@ -67,53 +106,77 @@ static int landisk_r8c_keys_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct device_node *np;
+	struct r8c_mcu *r8c;
 	struct r8c_keys *k;
-	struct r8c_key_device *kdev;
-	struct input_dev *idev;
 	unsigned short *keymap;
 	int keycnt;
-	int i = 0, ret;
+	int ret, i = 0;
+
+	r8c = dev_get_drvdata(dev->parent);
+	if (!r8c)
+		return -ENODEV;
 
 	keycnt = of_get_child_count(dev->of_node);
+	if (!keycnt)
+		return -ENODEV;
+
+	keymap = devm_kcalloc(dev, keycnt, sizeof(keymap[0]), GFP_KERNEL);
+	if (!keymap)
+		return -ENOMEM;
+
 	k = devm_kzalloc(dev, struct_size(k, keys, keycnt), GFP_KERNEL);
 	if (!k)
 		return -ENOMEM;
 
-	keymap = devm_kcalloc(dev, keycnt,
-			      sizeof(keymap[0]), GFP_KERNEL);
-	if (!keymap)
+	k->idev = devm_input_allocate_device(dev);
+	if (!k->idev)
 		return -ENOMEM;
+	k->r8c = r8c;
 
-	idev = devm_input_allocate_device(dev);
-	if (!idev)
-		return -ENOMEM;
-
-	input_set_drvdata(idev, k);
+	input_set_drvdata(k->idev, k);
 
 	for_each_child_of_node(dev->of_node, np) {
-		unsigned int type = EV_KEY;
-
-		kdev = &k->keys[i];
-		ret = of_property_read_string(np, "label", &kdev->name);
+		ret = of_property_read_string(np, "label", &k->keys[i].name);
 		if (!ret)
-			ret = of_property_read_u8(np, "iodata,landisk-r8c-evcode",
-						  &kdev->evcode);
+			ret = of_property_read_u8(np, "iodata,landisk-r8c-keycode",
+						  &k->keys[i].r8kcode);
 		if (!ret)
 			ret = of_property_read_u32(np, "linux,code",
-						  &kdev->lkcode);
+						   &k->keys[i].lkcode);
 		if (ret)
 			break;
-		ret = of_property_read_u32(np, "linux,input-type", &type);
-		if (ret == -EINVAL)
-			ret = 0;
-		else if (ret)
+		if (of_property_read_u32(np, "linux,input-type",
+					 &k->keys[i].evtype))
+			k->keys[i].evtype = EV_KEY;
+
+		switch(k->keys[i].r8kcode) {
+		case 'c':
+		case 'p':
+		case 'r':
 			break;
-		if (kdev->evcode < 'a' || 'z' < kdev->evcode) {
-			ret = -EINVAL;
+		case 'w':
+		case 'x':
+		case 'y':
+		case 'z':
+			if(of_device_is_compatible(dev->of_node,
+						   "iodata,landisk-r8c-keys-event"))
+			{
+				dev_err(dev,
+					"event for keycode '%c' is unavailable\n",
+					k->keys[i].r8kcode);
+				ret = -EINVAL;
+			}
+			break;
+		default:
+			dev_err(dev, "keycode '%c' is not supported\n",
+				k->keys[i].r8kcode);
+			ret = -ERANGE;
 			break;
 		}
-		keymap[i] = kdev->lkcode;
-		input_set_capability(idev, type, keymap[i]);
+		if (ret)
+			break;
+		keymap[i] = k->keys[i].lkcode;
+		input_set_capability(k->idev, k->keys[i].evtype, keymap[i]);
 		i++;
 	}
 
@@ -121,33 +184,52 @@ static int landisk_r8c_keys_probe(struct platform_device *pdev)
 	if (ret)
 		return ret;
 
-	idev->name = pdev->name;
-	idev->phys = "landisk-r8c-keys/input0";
-	idev->dev.parent = dev;
-	idev->id.bustype = BUS_HOST;
-	idev->id.vendor  = 0x0001;
-	idev->id.product = 0x0001;
-	idev->id.version = 0x0001;
+	k->keycnt = i;
 
-	idev->keycode = keymap;
-	idev->keycodesize = sizeof(keymap[0]);
-	idev->keycodemax = keycnt;
+	k->idev->name = pdev->name;
+	k->idev->phys = "landisk-r8c-keys/input0";
+	k->idev->dev.parent = dev;
+	k->idev->id.bustype = BUS_HOST;
+	k->idev->id.vendor  = 0x0001;
+	k->idev->id.product = 0x0001;
+	k->idev->id.version = 0x0001;
+	k->idev->keycode     = keymap;
+	k->idev->keycodesize = sizeof(keymap[0]);
+	k->idev->keycodemax  = keycnt;
 
-	k->idev = idev;
-	k->keycnt = keycnt;
-	k->last_code = 0x0;
-	k->nb.notifier_call = r8c_key_event;
-	k->nb.priority = 128;
+	if (of_device_is_compatible(dev->of_node,
+				    "iodata,landisk-r8c-keys-polled")) {
+		unsigned int interval;
 
-	ret = devm_landisk_r8c_register_event_notifier(dev, &k->nb);
+		if (of_property_read_u32(dev->of_node, "poll-interval", &interval))
+			interval = R8C_POLL_INTERVAL;
+		ret = input_setup_polling(k->idev, r8c_key_poll);
+		if (ret)
+			return ret;
+		input_set_poll_interval(k->idev, interval);
+	} else {
+		k->nb.notifier_call = r8c_key_event;
+		k->nb.priority = 128;
+		ret = devm_landisk_r8c_register_event_notifier(dev, &k->nb);
+		if (ret)
+			return ret;
+	}
+
+	ret = input_register_device(k->idev);
 	if (ret)
 		return ret;
 
-	return input_register_device(idev);
+	/* report the initial state of keys */
+	if (of_device_is_compatible(dev->of_node,
+				    "iodata,landisk-r8c-keys-polled"))
+		r8c_key_poll(k->idev);
+
+	return 0;
 }
 
 static const struct of_device_id landisk_r8c_keys_ids[] = {
-	{ .compatible = "iodata,landisk-r8c-keys" },
+	{ .compatible = "iodata,landisk-r8c-keys-event" },
+	{ .compatible = "iodata,landisk-r8c-keys-polled" },
 	{ /* sentinel */ },
 };
 MODULE_DEVICE_TABLE(of, landisk_r8c_keys_ids);
