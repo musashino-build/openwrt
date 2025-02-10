f2c4f761fdfbafe2b67393a38140116ff8af7c94
INAGAKI Hiroshi <musashino.open@gmail.com>
2025-02-11 00:34:29 +0900

    qualcommax: add support for ELECOM WRC-X3000GS2
    This is a combination of 32 commits.
    ------------------------------------
    
    qualcommax: add initial support for ELECOM WRC-X3000GS2
    
    qualcommax: fix nvmem binding for WRC-X3000GS2
    
    qualcommax: fix switch port configuration for WRC-X3000GS2
    
    qualcommax: ipq50xx: add sysupgrade support for WRC-X3000GS2
    
    qualcommax: set label to dp1 for WRC-X3000GS2
    
    qualcommax: ipq50xx: add LED configuration for WRC-X3000GS2
    
    qualcommax: update calibration-variant properties for WRC-X3000GS2
    
    qualcommax: adjust freq-limit properties for WRC-X3000GS2
    
    qualcommax: add coherent_pool=2M to bootargs for WRC-X3000GS2
    
    qualcommax: adjust qcom,*-addr of wifiN for WRC-X3000GS2
    
    qualcommax: ipq50xx: fixup caldata for WRC-X3000GS2
    
    qualcommax: add qcom,userpd-subsys-name to wifi0 for WRC-X3000GS2
    
    qualcommax: drop unnecessary status property from pd-N for WRC-X3000GS2
    
    qualcommax: add default triggers for wlan LEDs on WRC-X3000GS2
    
    qualcommax: remove phy-mode from dp2 on WRC-X3000GS2
    
    qualcommax: fix root= parameter in bootargs for WRC-X3000GS2
    
    qualcommax: ipq50xx: add seq to RAMFS_COPY_BIN for bootconfig_find_entry()
    
    qualcommax: ipq50xx: add comment about bootconfig_rw_index()
    
    qualcommax: ipq50xx: add error message on sysupgrade for WRC-X3000GS2
    
    qualcommax: add Build/mstc-header to ipq50xx.mk
    
    qualcommax: update recipe of factory.bin for WRC-X3000GS2
    
    qualcommax: set DEVICE_DTS_CONFIG to config@mp03.3 for WRC-X3000GS2
    
    qualcommax: fix IMAGE/factory.bin for WRC-X3000GS2
    
    qualcommax: move reset-gpio(s) of switch to DSA node for WRC-X3000GS2
    
    qualcommax: add missing pinmux of switch reset for WRC-X3000GS2
    
    qualcommax: remove unnecessary "cpu" label for WRC-X3000GS2
    
    qualcommax: ipq50xx: add smempart_next_root() to platform.sh
    
    qualcommax: ipq50xx: update sysupgrade script for WRC-X3000GS2
    
    qualcommax: add missing "router" switch for WRC-X3000GS2
    
    qualcommax: drop unused phy from mdio1 for WRC-X3000GS2
    
    qualcommax: drop unused qcom,halt-regs for WRC-X3000GS2
    
    qualcommax: move switch reset gpio to mdio for WRC-X3000GS2
    
    qualcommax: fix address of qca8337 for WRC-X3000GS2

--- target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
@@ -1,8 +1,105 @@
 PART_NAME=firmware
 REQUIRE_IMAGE_METADATA=1
 
-RAMFS_COPY_BIN='fw_printenv fw_setenv head'
+RAMFS_COPY_BIN='fw_printenv fw_setenv head seq'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+
+bootconfig_find_entry() {
+	local cfgbin="$1"
+	local partname="$2"
+	local i part parts offset
+
+	parts=$(hexdump -n 4 -s 8 -e '1/4 "%d"' "$cfgbin")
+	# partition count: <= 10
+	[ -z "$parts" ] || [ "$parts" = "0" ] || [ "$parts" -gt "10" ] && \
+		return 1
+
+	for i in $(seq 1 $parts); do
+		offset=$((0xc + 0x14 * (i - 1)))
+		part=$(dd if="$cfgbin" iflag=skip_bytes \
+			skip=$offset bs=16 count=1 2>/dev/null)
+		if [ "$part" = "$partname" ]; then
+			printf "0x%08x" $offset
+			return
+		fi
+	done
+
+	return 1
+}
+
+# Read or update an entry in Qualcomm bootconfig partition
+#
+# parameters:
+#   $1: partition name of bootconfig (ex.: "0:bootconfig", "0:bootconfig1", etc)
+#   $2: entry name in bootconfig (ex.: "0:hlos", "rootfs", etc)
+#   $3: index to set for the entry (0/1)
+#
+# operations:
+#   read : bootconfig_rw_index <bootconfig> <entry>
+#   write: bootconfig_rw_index <bootconfig> <entry> <index>
+bootconfig_rw_index() {
+	local bootcfg="$1"
+	local partname="$2"
+	local index="$3"
+	local mtddev
+	local offset
+	local current
+
+	case "$index" in
+	0|1|"") ;;
+	*) echo "invalid bootconfig index specified \"$index\""; return 1 ;;
+	esac
+
+	mtddev="$(find_mtd_part $bootcfg)"
+	[ -z "$mtddev" ] && \
+		return 1
+
+	dd if=$mtddev of=/tmp/${mtddev##*/} bs=1k
+
+	offset=$(bootconfig_find_entry "/tmp/${mtddev##*/}" $partname) || return 1
+	current=$(hexdump -n 4 -s $((offset + 0x10)) -e '1/4 "%d"' /tmp/${mtddev##*/})
+
+	[ -z "$index" ] && \
+		echo "$current" && return 0
+
+	if [ "$current" != "$index" ]; then
+		printf "\x$index" | \
+			dd of=$mtddev conv=notrunc bs=1 seek=$((offset + 0x10))
+	fi
+}
+
+# Qcom U-Boot always sets a name of current active partition to "rootfs" and
+# inactive partition is named as "rootfs_1", in smem partition table.
+# When the second partition is active, "rootfs" and "rootfs_1" are swapped.
+smempart_next_root() {
+	local index="$1"
+	local root_idx="$(find_mtd_index rootfs)"
+	local root1_idx="$(find_mtd_index rootfs_1)"
+	local root_offset root1_offset
+
+	[ -z "$root_idx" ] || [ -z "$root1_idx" ] && \
+		return 1
+
+	root_offset=$(cat /sys/block/mtdblock$root_idx/device/offset)
+	root1_offset=$(cat /sys/block/mtdblock$root1_idx/device/offset)
+
+	case "$index" in
+	0)
+		[ "$root_offset" -lt "$root1_offset" ] && \
+			echo "rootfs" || \
+			echo "rootfs_1"
+		;;
+	1)
+		[ "$root_offset" -lt "$root1_offset" ] && \
+			echo "rootfs_1" || \
+			echo "rootfs"
+		;;
+	*)
+		v "invalid index specified..."
+		return 1
+		;;
+	esac
+}
 
 remove_oem_ubi_volume() {
 	local oem_volume_name="$1"
@@ -72,6 +169,24 @@ platform_check_image() {
 
 platform_do_upgrade() {
 	case "$(board_name)" in
+	elecom,wrc-x3000gs2)
+		local delay index
+
+		delay=$(fw_printenv bootdelay)
+		[ -z "$delay" ] || [ "$delay" -eq "0" ] && \
+			fw_setenv bootdelay 3
+
+		index=$(bootconfig_rw_index "0:bootconfig" rootfs) || \
+			v "failed to read bootconfig index..." && \
+			nand_do_upgrade_failed
+		CI_UBIPART=$(smempart_next_root $index) || \
+			v "failed to get next root..." && \
+			nand_do_upgrade_failed
+
+		remove_oem_ubi_volume ubi_rootfs
+		remove_oem_ubi_volume wifi_fw
+		nand_do_upgrade "$1"
+		;;
 	linksys,mx2000|\
 	linksys,mx5500|\
 	linksys,spnmx56)
