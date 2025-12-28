24609f672ffb93be7d3706543c08fe8680c3f6ab
INAGAKI Hiroshi <musashino.open@gmail.com>
2025-12-29 02:11:02 +0900

    kirkwood: nas: built-in ext4 FS support

--- target/linux/kirkwood/nas/target.mk
+++ target/linux/kirkwood/nas/target.mk
@@ -2,7 +2,7 @@ SUBTARGET:=nas
 DEVICE_TYPE:=nas
 BOARDNAME:=NAS Devices
 FEATURES+=ext4 boot-part rootfs-part
-DEFAULT_PACKAGES+=kmod-fs-ext4 kmod-hwmon-drivetemp e2fsprogs partx-utils
+DEFAULT_PACKAGES+=kmod-hwmon-drivetemp e2fsprogs partx-utils
 
 define Target/Description
 	Build firmware images for NAS devices which boot from SATA.
